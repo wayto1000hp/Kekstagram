98c329c17f491ad4a32c6068e32948635d144cee
wayto1000hp <ipsum1999@gmail.com>
2022-10-21 21:57:37 +0800

    fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-let checkStringLength = function () {
-    
-  };
+let checkStringLength = function (stringLength,maxLength) {
+  if (stringLength>maxLength){
+  console.log('True')
+  } else {
+    console.log('False')
+  }
+};
