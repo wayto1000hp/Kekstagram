8db94991acc0d75fbccfe7f2d29582c2a8beb844
wayto1000hp <ipsum1999@gmail.com>
2022-10-22 14:46:57 +0800

    Final

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-let checkStringLength = function (stringLength,maxLength) {
-  if (stringLength>maxLength){
-  console.log('True')
-  } else {
-    console.log('False')
+const getSum = (a,b) => {
+  if (a<0||b<0||a>b){
+    return NaN
   }
-};
+  a = Math.ceil(a);
+  b = Math.floor(b);
+  return Math.floor(Math.random() * (b - a + 1)) + a;;
+}
+
+const MAX_LENGTH = 140;
+const compareLength = (str,maxLength) => str.length < maxLength;
+compareLength('qwerty', MAX_LENGTH)
+
+let getRandomNumber = function  (numberOne, numberTwo) {
+
+}
