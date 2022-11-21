487c9e43154734bc715d915f83acf1717bbfccb9
wayto1000hp <ipsum1999@gmail.com>
2022-11-21 23:44:37 +0800

    fix

--- js/messages.js
+++ js/messages.js
@@ -2,54 +2,45 @@ import {isEscapeKey} from './util.js';
 
 const errorMessageTemplate = document.querySelector('#error').content.querySelector('.error');
 const successMessageTemplate = document.querySelector('#success').content.querySelector('.success');
-const successMessageElement = successMessageTemplate.cloneNode(true);
-const errorMessageElement = errorMessageTemplate.cloneNode(true);
-const innerSuccess = document.querySelector('.success__inner');
-const innerError = document.querySelector('.error__inner');
 
-const onMessageEscKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
+const showMessage = (messageTemplate) => {
+  const messageElement = messageTemplate.cloneNode(true);
+  const closeButton = messageElement.querySelector('.button');
+
+  function hideMessage () {
+    messageElement.remove();
+  }
+
+  const onEscKeydown = (evt) => {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      evt.stopPropagation();
+      document.removeEventListener('keydown', onEscKeydown);
+      hideMessage();
+    }
+  };
+
+  const onMaskClick = (evt) => {
+    if (evt.target.nodeName === 'SECTION') {
+      messageElement.removeEventListener('click', onMaskClick);
+      hideMessage();
+    }
+  };
+
+  const onCloseButtonClick = () => {
+    closeButton.removeEventListener('click', onCloseButtonClick);
     hideMessage();
-  }
-};
-const onErrorButtonClick = () => {errorMessageElement.addEventListener('click', (evt) => {
-  if (evt.target === innerError){
-    return;
-  }
-  hideMessage();
-});
-};
-const onSuccessButtonClick = () => {errorMessageElement.addEventListener('click', (evt) => {
-  if (evt.target === innerSuccess){
-    return;
-  }
-  hideMessage();
-});
-};
-const showSuccessMessage = () => {
-  document.addEventListener('keydown', onMessageEscKeydown);
-  successMessageElement.addEventListener('click', onErrorButtonClick);
-  successMessageElement.querySelector('.success__button').addEventListener('click', onSuccessButtonClick);
-  document.querySelector('body').append(successMessageElement);
+  };
+
+  document.addEventListener('keydown', onEscKeydown);
+  messageElement.addEventListener('click', onMaskClick);
+  closeButton.addEventListener('click', onCloseButtonClick);
+  document.body.append(messageElement);
 };
 
-const showErrorMessage = () => {
-  document.addEventListener('keydown', onMessageEscKeydown);
-  errorMessageElement.addEventListener('click', onErrorButtonClick);
-  errorMessageElement.querySelector('.error__button').addEventListener('click', onErrorButtonClick);
-  document.querySelector('body').append(errorMessageElement);
-};
-
-function hideMessage () {
-  const messageElement = document.querySelector('.success') || document.querySelector('.error');
-  messageElement.remove();
-  successMessageElement.removeEventListener('click', onErrorButtonClick);
-  errorMessageElement.removeEventListener('click', onErrorButtonClick);
-  document.removeEventListener('keydown', onMessageEscKeydown);
-  errorMessageElement.querySelector('.error__button').removeEventListener('click', onErrorButtonClick);
-  successMessageElement.querySelector('.success__button').addEventListener('click', onErrorButtonClick);
-}
-
+const showErrorMessage = () => showMessage(errorMessageTemplate);
+const showSuccessMessage = () => showMessage(successMessageTemplate);
 
 export { showErrorMessage, showSuccessMessage };
+
+
