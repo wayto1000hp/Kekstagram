0b1cee849273d55d4efe38bafdbd640ef8bc3fef
wayto1000hp <ipsum1999@gmail.com>
2022-11-22 15:06:00 +0800

    module11-task1 fix clean

--- js/util.js
+++ js/util.js
@@ -21,20 +21,6 @@ const showErrorAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-const getRandomNumber = (a,b) => {
-  if (a < 0 || b < 0 || a > b) {
-    return NaN;
-  }
-  a = Math.ceil(a);
-  b = Math.floor(b);
-  return Math.floor(Math.random() * (b - a + 1)) + a;
-};
-
-
-const compareLength = (str,maxLength) => str.length < maxLength;
-
-const getRandomArrayElement = (array) => array[getRandomNumber(0,array.length - 1)];
-
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export { getRandomArrayElement,compareLength,getRandomNumber,isEscapeKey, showErrorAlert };
+export { isEscapeKey, showErrorAlert };
